08bb053d9ab31cbb3fc530523f457aaa7b8668cc
lorenzofox3 <laurent34azerty@gmail.com>
2018-01-22 10:27:41 -0500

    better module packaging

--- rollup.conf.js
+++ rollup.conf.js
@@ -3,7 +3,6 @@ export default {
 	output:[
 		{format:'es',file:'./dist/index.mjs'},
 		{format:'cjs',file:'./dist/index.js'},
-		{format:'iife',name:'smartTableOperators',file:'./dist/smart-table-operators.js', sourcemap:true},
-		{format:'es',file:'./dist/smart-table-operators.es.js', sourcemap:true}
+		{format:'iife',name:'smartTableOperators',file:'./dist/smart-table-operators.js', sourcemap:true}
 	],
 }
