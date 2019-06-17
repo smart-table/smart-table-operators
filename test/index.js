aecbec4d10753c7f30ec43a4c305ea94dd467382
Laurent Renard <laurent@Laurents-MacBook-Pro.local>
2019-06-17 11:29:29 -0400

    bump dev dependencies

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
-import test from 'zora';
-import {swap, tap, curry, compose, apply} from '../dist/bundle/module';
+import {test} from 'zora';
+import {apply, compose, curry, swap, tap} from '../dist/bundle/module';
 
 const substract = (a, b) => a - b;
 const addTwo = (a) => a + 2;
